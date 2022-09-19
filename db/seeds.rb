9b2465be40f64f042bfbd980c55c188e1e88fbc8
Alan Garcia <alangarciacoding@gmail.com>
2022-09-19 18:46:01 -0500

    Create models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.create(name: "Jeremy", email: "dimes@test.com", password: "password", image: "gsdanf")
+User.create(name: "Gita", email: "gita@test.com", password: "cat", image: "sgasdg")
+User.create(name: "Alan", email: "alan@test.com", password: "dog", image: "dsafd")
+
+Relationship.create(leader_id: 2, follower_id: 1)
+Relationship.create(leader_id: 2, follower_id: 3)
+Relationship.create(leader_id: 1, follower_id: 2)
+
+Tweet.create(user_id: 2, text: "Omg hi")
+Tweet.create(user_id: 2, text: "Lol")
+Tweet.create(user_id: 2, text: "I had cereal today. Yum.")
+Tweet.create(user_id: 1, text: "Omg hey")
