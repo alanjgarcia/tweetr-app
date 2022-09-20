b32ff61db0ba70b4c0f8890cfcaf83015d4982a2
gitalevin <reachglevin@gmail.com>
2022-09-19 21:26:16 -0500

    created relationship controller create and index

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,19 @@ Rails.application.routes.draw do
   get "/login" => "sessions#new"
   post "/sessions" => "sessions#create"
   get "/logout" => "sessions#destroy"
+
+  # resources :tweets
+
+  # Automatically generates the following routes
+  # get "/tweets" => "tweets#index", as: "tweets"
+  # get "/tweets/new" => "tweets#new", as: "new_tweet"
+  # post "/tweets" => "tweets#create"
+  # get "/tweets/:id" => "tweets#show", as: "tweet"
+  # get "/tweets/:id/edit" => "tweets#edit", as: "edit_tweet"
+  # patch "/tweets/:id" => "tweets#update"
+  # delete "/tweets/:id" => "tweets#destroy"
+
+  # get "/relationship" => "relationship#index"
+  # post "/relationships" => "relationships#create"
+  resources :relationships
 end
